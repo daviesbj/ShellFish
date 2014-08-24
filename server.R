f57086b202e79ad933317791e5653545a475e05f
daviesbj <daviesbj@bp.com>
2014-08-24 20:52:42 +0100

    As-deployed version of app

--- server.R
+++ server.R
@@ -1,30 +1,35 @@
 library(shiny)
 library(ggplot2)
 
-load( 'data/land.Rdata' )
+load( 'data/hland.Rdata' )
 
 shinyServer(function(input, output) {
   
   plotData <- reactive(function() {
-    land[ (land$species==input$species)&(land$state==input$state), ]
+    hland[ (hland$species==input$species)&(hland$state==input$state), ]
   })
   
   output$speciesChooser <- renderUI({
     selectInput( 'species', 'Species',
-                 levels(factor(land$species[land$state==input$state])))
+                 levels(factor(hland$species[hland$state==input$state])))
   })
   
   output$plot <- reactivePlot(function() {
     
-    p <- ggplot( plotData(), aes( date, tons ) )
+    p <- ggplot( plotData(), aes( date.x, tons ) )
     p <- p + geom_line()
-    p<-p + xlim( as.Date( c( '1990-01-01', '2012-12-01' ) ) )
-    p<-p+scale_x_date( breaks = as.Date( paste0( as.character( seq(1990,2013 ) ),
+    p <- p + geom_point( aes( size = level, color = type ) )
+    p <- p + geom_text( aes( label = name ) )
+    p <- p + xlim( as.Date( c( '1990-01-01', '2012-12-01' ) ) )
+    p <- p + scale_x_date( breaks = as.Date( paste0( as.character( seq(1990,2013 ) ),
                                                  '-01-01' ) ),
-                       labels = as.character( seq( 1990, 2013 ) ) )
+                           labels = as.character( seq( 1990, 2013 ) ) )
+    p <- p + expand_limits( y = 0 )
+    p <- p + xlab( 'Date' ) + ylab( 'Tons Landed Per Month' )
+    # p <- p + main( 'Main Title' )
     
     print(p)
     
-  }, height=700)
+  }, height = 700 )
   
 })
--- ui.R
+++ ui.R
@@ -1,26 +1,42 @@
 library(shiny)
 library(ggplot2)
 
-load( 'data/land.Rdata' )
+load( 'data/hland.Rdata' )
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Atlantic/Gulf Fisheries Explorer"),
+  headerPanel("Atlantic/Gulf Fishery Incident Explorer"),
   
   sidebarPanel(
     
     selectInput( 'state',   'State',
-                 levels( land$state ),
+                 levels( hland$state ),
                  selected ='Louisiana'  ),
     
     # selectInput( 'species', 'Species',
-    #              levels( land$species ),
+    #              levels( hland$species ),
     #              selected = 'OYSTER, EASTERN' )
-    uiOutput('speciesChooser')
+    
+    uiOutput( 'speciesChooser' ),
+    
+    h3( "Instructions" ),
+    p( 'Choose a state and a species.' ),
+    p( paste0( 'The application will use your input to select the data to plot from ',
+               'its underlying data tables. As a convenience, it also uses your ',
+               'State selection to restrict the list of species to those fished in the state')
+    ),
+    p( 'Note to markers -- this is how the application processes your input' ),
+    p( 'A good one to try is Louisiana and OYSTER, EASTERN' ),
+    p( paste0( 'The graph shows the available fishery landings history for your ',
+               'selected species and state, with named hurricanes and other incidents that hit ',
+               'the state during the period 1990-2012 that may have affected fishery activity, ',
+               'coded by color and symbol size to indicate magnitude' )
+    ),
+    p( 'The graph looks better if you stretch the window horizontally' )
 
   ),
   
   mainPanel(
-    plotOutput('plot')
+    plotOutput( 'plot' )
   )
 ))
